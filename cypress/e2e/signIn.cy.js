e49035013bf3ed64246652506e5076e703954b29
Heorhii Savostikov <georgesavostikov@gmail.com>
2026-03-30 19:29:09 +0300

    Added some new changes

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,22 +1,16 @@
 /// <reference types='cypress' />
 
-const { generateUser } = require('../support/generateUser');
-
 describe('Sign in page', () => {
-  beforeEach(() => {
-    cy.visit('/user/login');
-  });
-
   it('should login user', () => {
-    const { email, password, username } = generateUser();
+    cy.visit('https://conduit.mate.academy/user/login');
 
     cy.get('h1').should('contain.text', 'Sign in');
 
-    cy.get('[placeholder=Email]').type(email);
-    cy.get('[placeholder=Password]').type(password);
+    cy.get('[placeholder=Email]').type('george2025@gmail.com');
+    cy.get('[placeholder=Password]').type('Password2025');
 
-    cy.get('.btn').should('exist').click();
+    cy.get('.btn').click();
 
-    cy.get('.nav-link').should('contain.text', `${username}`);
+    cy.get('.nav-link').should('contain.text', `george2025`);
   });
 });
